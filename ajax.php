e228644142a7c1ac63fea0cf6e7b6f263a487cbb
Likik1 <luky.holub@gmail.com>
2020-01-23 08:55:29 +0100

    ajax
    tak jsem uspěšně zapoměl hodit komit

--- ajax.php
+++ ajax.php
@@ -1,6 +1,5 @@
 <?php
 require_once("../config.php");
-
 $data = $_GET["data"];
 
 if(empty(trim($data)))
@@ -9,8 +8,10 @@ if(empty(trim($data)))
 $spojeni = mysqli_connect(dbhost, dbuser, dbpass, dbname);
 $pocet =  mysqli_query($spojeni,"SELECT `pocet` FROM `Kalkulacka_jason` WHERE `klavesa` = $data");
 if(empty(trim($data)))
-    mysqli_query($spojeni,"INSERT INTO `Kalkulacka_jason`(`klavesa`, `pocet`) VALUES ($data, 1)");
+    mysqli_query($spojeni,"INSERT INTO `Kalkulacka_jason`(`klavesa`, `pocet`) VALUES ($data, "1")");
 else
+{
     mysqli_query($spojeni,"UPDATE `Kalkulacka_jason` SET `klavesa`= $data, `pocet` = $pocet + 1 WHERE `klavesa` = $data");
+}
 
 mysqli_close($spojeni);
